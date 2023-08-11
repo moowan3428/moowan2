37dfc59860962721c9f8a50088abb07f4704fb12
moowan3428 <makin668899@gmail.com>
2023-08-11 09:51:22 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+function sortByFrequencyAndValue(inputArray) {
+    const frequencyMap = new Map();
+    // Count the frequency of each number
+    for (const num of inputArray) {
+        frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+    }
+    // Sort the array based on frequency and then value
+    const sortedArray = inputArray.sort((a, b) => {
+        const freqComparison = frequencyMap.get(a) - frequencyMap.get(b);
+        if (freqComparison !== 0) {
+            return freqComparison;
+        }
+        return a - b;
+    });
+    return sortedArray;
+}
+const input = [2, 3, 4, 4, 34, 6, 7, 2, 3, 7, 8, 8, 8, 7, 9, 10, 41, 8];
+const sortedOutput = sortByFrequencyAndValue(input);
+console.log(sortedOutput);
